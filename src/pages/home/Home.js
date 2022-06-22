e88b1c2e490fa4c54c80e69c1a7a0adb5874936b
masoud <masoudmotahari1380@gmail.com>
2022-06-22 09:06:36 -0700

    add useTitle (custom hook)

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Banner from "../../components/Banner";
 import TransporterButton from "../../components/TransporterButton";
+import useTitle from "./../../hook/useTitle";
 
 const Home = () => {
+  useTitle("Home");
   return (
     <div className="pb-5">
       <Banner />
