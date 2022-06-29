53bfe0faeed234d0d1ae0a28e0c0a2bd7b9ca030
masoud <masoudmotahari1380@gmail.com>
2022-06-29 08:59:13 -0700

    apply tailwind menu link class to css file

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -9,26 +9,17 @@ const MenuItems = () => {
       <span className="h-0.5 w-5/6 bg-menu/40"></span>
       <div className="mt-4 flex flex-col gap-2 ">
         <div className="relative">
-          <Link
-            className="text-menu after:absolute after:bottom-0 after:left-0  after:h-0.5 after:w-0  after:bg-white/70 after:rounded-xl after:hover:left-1 after:transition-all after:duration-500 after:ease-in after:content-[''] after:hover:w-3/4"
-            to={"/"}
-          >
+          <Link className="menuLink" to={"/"}>
             Home
           </Link>
         </div>
         <div className="relative">
-          <Link
-            className="text-menu after:absolute after:bottom-0 after:left-0  after:h-0.5 after:w-0  after:bg-white/70 after:rounded-xl after:hover:left-1 after:transition-all after:duration-500 after:ease-in after:content-[''] after:hover:w-3/4"
-            to={"/Users"}
-          >
+          <Link className="menuLink" to={"/Users"}>
             Users
           </Link>
         </div>
         <div className="relative">
-          <Link
-            className="text-menu after:absolute after:bottom-0 after:left-0  after:h-0.5 after:w-0  after:bg-white/70 after:rounded-xl after:hover:left-1 after:transition-all after:duration-500 after:ease-in after:content-[''] after:hover:w-3/4"
-            to={"/Products"}
-          >
+          <Link className="menuLink" to={"/Products"}>
             Products
           </Link>
         </div>
